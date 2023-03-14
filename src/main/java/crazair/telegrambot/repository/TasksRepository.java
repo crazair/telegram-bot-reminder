00f00e0f2fd4e379e723804bb3abad04d912f5c3
crazair <crazzyzyzy@rambler.ru>
2023-03-14 15:26:02 +0300

    готово

--- src/main/java/crazair/telegrambot/repository/TasksRepository.java
+++ src/main/java/crazair/telegrambot/repository/TasksRepository.java
@@ -2,10 +2,12 @@ package crazair.telegrambot.repository;
 
 import crazair.telegrambot.model.NotificationTasks;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
 
+@Repository
 public interface TasksRepository extends JpaRepository<NotificationTasks, Long> {
     Collection<NotificationTasks> findAllByDateTime(LocalDateTime dateTime);
 }
